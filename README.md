0571bf160403033b67043d1550f71efffced0f20
abumuawiyah <ibnuyazit@gmail.com>
2021-12-05 15:28:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-f8klvf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-f8klvf)
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+@Component({
+  selector: 'test',
+  templateUrl: './table.component.html',
+})
+export class TableComponent implements OnInit {
+ngOnInit(): void {
+console.log(this)
+}
+  @Input() rows = [];
+  @Input() columns = [];
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
+}
